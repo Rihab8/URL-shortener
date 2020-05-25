beaac425353536d7cb56c6fb4faa3bf5380c5b65
RIHAB KASIM <rihabkasim.rk@gmail.com>
2020-05-25 17:19:34 +0530

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const mongodb = () => {
   mongoose.connect(
-    "mongodb+srv://rihab:12345@cluster0-tkl3u.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://rkurl:1234@cluster0-tkl3u.mongodb.net/test?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
